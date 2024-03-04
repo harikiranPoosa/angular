5e0e971b7623e7adf9739361dceb10008ac91501
harikiranPoosa <156776643+harikiranPoosa@users.noreply.github.com>
2024-03-03 18:51:19 -0800

    Create Book.js

---
+++ node-backend/model/Book.js
@@ -0,0 +1,16 @@
+//now here going to create schema 
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+let Book = new Schema({
+    name:{
+        type:String
+    },
+    price:{
+        type:String
+    },
+    description:{
+        type:String
+    },
+},{collection:'books'})
+module.exports=mongoose.model('Book',Book)
